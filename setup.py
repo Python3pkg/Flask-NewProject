624d2320cc175d8bf4c551b7dda1c266e8245365
Raymond Williams <GunaksGit@gmail.com>
2016-10-27 18:29:25 -0700

    Modified setup and renamed directories

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='Flask-NewProject',
+      version='0.0.0',
+      description='Create new Flask project.',
+      url='https://github.com/Gunak/Flask-NewProject/',
+      author='Raymond Williams',
+      author_email='Raymond.n.williams86@gmail.com',
+      license='MIT',
+      packages=['flask-project'],
+      zip_safe=False)
