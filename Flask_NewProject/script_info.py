587e3032a0603a8271f6acdce208cf73b3bd69bf
= <GunaksGit@gmail.com>
2016-10-31 12:55:34 -0700

    added basic testing doc string

--- Flask_NewProject/script_info.py
+++ Flask_NewProject/script_info.py
@@ -21,4 +21,25 @@ def index_page():
 if __name__ == "__main__":
     app.run()
 """
-    
+
+def basic_test_for_flask():
+    return """from app import app
+import unittest
+
+class FlaskBasicTests(unittest.TestCase):
+
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True
+
+    def tearDown(self):
+        pass
+
+    def test_for_index_response(self):
+        result = self.app.get('/')
+        self.assertEqual(result.status_code, 200)
+
+    def test_for_hello_world(self):
+        result = self.app.get('/')
+        self.assertEqual(result.data, "Hello World!")
+"""
