6b4fd18e28515429a4b20ad8d9bd5f74f2e02b55
= <GunaksGit@gmail.com>
2016-10-31 12:36:31 -0700

    added app content for a simple app.py file

--- Flask_NewProject/script_info.py
+++ Flask_NewProject/script_info.py
@@ -6,3 +6,19 @@ if __name__ == "__main__":
 app.run()""".format(projectName)
 
     return run
+
+
+def app_simple():
+    return """from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index_page():
+    return 'Hello World!'
+
+if __name__ == "__main__":
+    app.run()
+"""
+    
